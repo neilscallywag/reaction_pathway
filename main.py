2b6b1a7f69d803192189b5ee0800b7ac45864a9a
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-08-13 11:58:14 +0800

    Added Documentation

--- main.py
+++ main.py
@@ -1,106 +1,39 @@
 import requests
 import random
-import operator
 import networkx as nx
 import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-import yaml
-from collections import defaultdict
-from pprint import pprint
 
+
+'''
+    Valency Table:
+    list of period table so that it can be used to determine
+    valency of each atom
+'''
 group_13 = ['B','Al', 'Ga','In']
 group_14 = ['C','c','Si','Ge','Sn','Pb']
 group_15 = ['N','n','P','As']
 group_16 = ['O','o','S','s','Se']
 group_17 = ['F','Cl','Br','I']
-class molecule:
-    def __init__(self):
-        self.molecule = {}
-    def __iter__(self):
-        for each in self.molecule.values():
-            yield each    
-    def add_atom(self,atom):
-            self.molecule[atom] = [] 
-    def graph(self):
-        d = {}
-        for k,v in self.molecule.items():
-            z = {x[0].index: {'bond-order':x[1]} for x in v}
-            d[k.index] = z
-        return d
-    def schema(self):
-        return self.molecule         
-    def add_bond(self,u,v,w=1):
-        self.molecule[u].append((v,w))
-        self.molecule[v].append((u,w))
-    def nodes(self):
-        return [x.attributes() for x in self.molecule.keys()]
-    def bonds(self):
-        for keys in self.molecule:
-            pp.pprint((keys, self.molecule[keys]))
-    def get_surrounding_atoms(self,atom):
-        current = len(self.molecule[atom])
-        if atom in self.molecule[atom]:
-            current += 1
-        return current
-    def get_atom(self,index):
-        k = list(self.molecule)
-        key = k[index]
-        return key
-    def get_valence(self,index):
-        k = list(self.molecule)
-        key = k[index]
-        return key.valency        
-    def bond_sum(self,atom):
-        s = 0
-        for x,y in self.molecule[atom]:
-            s += y
-        return s
-    def HC(self,atom):
-        s = self.bond_sum(atom)
-        return atom.valency - s
-    def add_hydrogens(self):
-        last_atom_index = list(self.molecule)[-1]
-        counter = last_atom_index.index + 1
-        f = self.molecule.copy()
-        for k in f.keys():
-            hydrogen_count = self.HC(k)
-            for hydrogen_index in range(hydrogen_count):
-                self.add_atom(atom(counter,'H'))
-                self.add_bond(self.get_atom(k.index),self.get_atom(counter), 1)
-                counter +=1        
-    def fix_valence(self):
-        pass
-    def fix_charge(self): #currently charges are not included 
-        pass
-    def fix_aromatic_bonds(self): #currently all aromatic bonds are either order 1 or 2
-        pass
-    def carboxyl(self) -> bool:
-        c = 0
-        for atom in self.molecule.keys():
-            for value in self.molecule[atom]:
-                print(value[0].name , value[1])
-                if (value[0].name == 'O' and value[1] == 2 and (atom.name == 'C' or atom.name == 'c')) or (value[0].name == 'C' and value[1] == 2 and (atom.name == 'O')):
-                    c +=1
-        if c == 2:
-            return print('y')
-        else:
-            return print('n')
-                    
-                    
-                
 
-        #for atom in self.molecule:
-         #   if atom.name == "C":
-          #      for values in atom:
-           #         if values[0].name == "O" and values[1] == 2:
-            #            return True
-             #       else:
-              #          return False
-                        
 
+'''
+    Defining an Atom:
+                     An Atom for our purpose is defined as an object with the
+                     following properties:
+                     1. An index -> index: int
+                     2. Its Chemical Symbol -> name: char
+                     3. Its Valency - > valency: int
+                     4. Its Charge -> Charge: undefined
+                     5. If the atom is aromatic or not -> aromatic: Boolean
+
+                     Extra Information:
+                     Aromaticity is currently defined by lower case strings
+                     according to SMILES format
+
+'''
 
 class atom(object):
     def __init__(self,index,name,charge=0):
@@ -109,8 +42,6 @@ class atom(object):
         self.valency = self.get_valency(name)
         self.charge = charge
         self.aromatic = True if name.islower() else False
-    def attributes(self):
-       return None
     def valency(self):
         return self.valency
     def index(self):
@@ -119,6 +50,9 @@ class atom(object):
         return self.name
     def aromatic(self):
         return self.aromatic
+    '''
+        Using the valency table above to get valency
+    '''
     def get_valency(self,name):
         if name in group_13:
             return 3
@@ -132,9 +66,268 @@ class atom(object):
             return 1
         else:
             return 0
+
+
+
+
+    '''
+        Defining a Molecule:
+        Molecule is represented using a modifed adjacency list structure
+        Each Node and adjacent Nodes are labelled by the Atom object
+
+
+        Index Representation of Methane (CH4)(Smiles:C)
+
+            {
+          "0": {
+            "1": {
+              "bond-order": 1
+            },
+            "2": {
+              "bond-order": 1
+            },
+            "3": {
+              "bond-order": 1
+            },
+            "4": {
+              "bond-order": 1
+            }
+          },
+          "1": {
+            "0": {
+              "bond-order": 1
+            }
+          },
+          "2": {
+            "0": {
+              "bond-order": 1
+            }
+          },
+          "3": {
+            "0": {
+              "bond-order": 1
+            }
+          },
+          "4": {
+            "0": {
+              "bond-order": 1
+            }
+          }
+        }
+
+        
+
+        Object Representation of Methane:
+
+        
+        {
+        <__main__.atom object at ADDR>: [(<__main__.atom object at ADDR>,1)],
+        
+        <__main__.atom object at ADDR>: [(<__main__.atom object at ADDR>,1)],
+        
+        <__main__.atom object at ADDR>: [(<__main__.atom object at ADDR>,1)],
+        
+        <__main__.atom object at ADDR>: [(<__main__.atom object at ADDR>,1),
+                                         (<__main__.atom object at ADDR>,1),
+                                         (<__main__.atom object at ADDR>,1),
+                                         (<__main__.atom object at ADDR>,1)],
+                                                       
+        <__main__.atom object at ADDR>: [(<__main__.atom object at ADDR>,1)]
+        }
+
+
+    '''
+
+class molecule:
+    def __init__(self):
+        self.molecule = {}
+    def __iter__(self):
+        for each in self.molecule.values():
+            yield each
+
+    '''
+        Adding an ATOM in an empty dictionary as Key with empty list as its values.
+        Indicating no adjacent atoms.
+
+        Input is an ATOM object
+        Output: NIL
+    '''
+
+    def add_atom(self,atom):
+            self.molecule[atom] = []
+
+    '''
+        Printing Index Notation of the atom.
+    '''
+    def graph(self):
+        d = {}
+        for k,v in self.molecule.items():
+            z = {x[0].index: {'bond-order':x[1]} for x in v}
+            d[k.index] = z
+        return d
+
+    '''
+        Printing the Object Representation of the Molecule
+    '''
+    def schema(self):
+            return self.molecule
+
+    '''
+        Returns list of nodes
+    '''
+
+    def nodes(self):
+        return [x.name for x in self.molecule.keys()]
+
+    '''
+        Adding a bond and the bond order between the atoms. Inputs are all in ATOM object.
+        u: starting atom
+        v: ending atom
+        w: is the bond order. If bond order is not specified, default single bond is added
+    '''
+    
+    def add_bond(self,u,v,w=1):
+        self.molecule[u].append((v,w))
+        self.molecule[v].append((u,w))
+        
+
+    '''
+        Getting the number of surrounding atoms.
+        It will be later used to calculate remaining valency of the attached atom so that Hydrogens can be added.
+
+        Input is the ATOM Object
+        Output is an int value.
+    '''
+    def get_surrounding_atoms(self,atom):
+        current = len(self.molecule[atom])
+        if atom in self.molecule[atom]:
+            current += 1
+        return current
+
+    '''
+        It is not possible for us to input an object for the methods described above.
+        Hence we need to use the Index to make reference to the Atom object.
+
+        This method returns the Atom based on its index attribute.
+    '''
+
+    def get_atom(self,index):
+        k = list(self.molecule)
+        key = k[index]
+        return key
+
+
+    '''
+        Now we will start adding Hydrogens to our Molecule based on remaining valency
+
+        We will get the valency of each ATOM currently in the Molecule.
+        Next, we will calculate the sum of all the bond orders in the surrounding atoms.
+        Lastly, we will minus off the sum of bond orders from the valency to get the number of hydrogens (Hydrogen Count) we can attach to that Atom.
+
+    '''
+
+    def get_valence(self,index):
+        k = list(self.molecule)
+        key = k[index]
+        return key.valency        
+    def bond_sum(self,atom):
+        s = 0
+        for x,y in self.molecule[atom]:
+            s += y
+        return s
+    def HC(self,atom):
+        s = self.bond_sum(atom)
+        return atom.valency - s
+
+    '''
+        Adding the hydrogens
+
+        1. Get the last index of the Hydrogen-Less molecule and + 1 to indicate the Atom index of the first Hydrogen
+        2. Create New Atom for each Hydrogen and attach it using the methods described above
+
+    '''
+    def add_hydrogens(self):
+        last_atom_index = list(self.molecule)[-1]
+        counter = last_atom_index.index + 1
+        f = self.molecule.copy()
+        for k in f.keys():
+            hydrogen_count = self.HC(k)
+            for hydrogen_index in range(hydrogen_count):
+                self.add_atom(atom(counter,'H'))
+                self.add_bond(self.get_atom(k.index),self.get_atom(counter), 1)
+                counter +=1
+
+
+    '''
+        Finally, Converting the SMILES formatted string to join atoms, branches, rings and generating the molecule itself.
+
+    '''
+
+    def smile(self,z):
+        anchor = None
+        index = 0
+        next_bond = None
+        branches= []
+        default = 1
+        stack = tokenise(z)
+        rings = {}
+        for tokentype,token in stack:
+            if tokentype == 'atom':
+                self.add_atom(atom(index,token))
+                if anchor is not None:
+                    if next_bond is None:
+                        next_bond = default
+                    if next_bond:
+                        self.add_bond(self.get_atom(anchor),self.get_atom(index), next_bond)
+                    next_bond = None
+                anchor = index
+                index += 1
+            elif tokentype == 'sb':
+                branches.append(anchor)
+            elif tokentype == 'eb':
+                anchor = branches.pop()
+            elif tokentype == 'bond':
+                next_bond = token
+            elif tokentype == 'ring':
+                if token in rings:
+                    jdx, order = rings[token]
+                    if next_bond is None and order is None:  
+                        next_bond = 1
+                    elif order is None:
+                        next_bond = 1
+                    elif next_bond is None:
+                        next_bond = order
+                    elif next_bond != order:
+                        print("error")
+                        break
+                    elif int(index - 1) == jdx:
+                        print("error2")
+                    if next_bond is not None:
+                        self.add_bond(self.get_atom(index-1),self.get_atom(jdx),next_bond)
+                        next_bond = None
+                    next_bond = None
+                    del rings[token]
+                else:
+                    rings[token] = (index-1,next_bond)     
+        return self.add_hydrogens()
+
+    
+
+    '''
+                    End Molecule Class
+
+
+                    
+                    Interpreting a SMILES string. The code below is inspired by the PySMILES library.
+
+                    Input is a SMILES string.
+
+    '''
+
+
 def tokenise(smiles):
     o = 'B C N O P S F Cl Br I * b c n o s p'.split()
-    smiles = iter(z)
+    smiles = iter(smiles)
     queue =[]
     token = '' 
     peek = None
@@ -177,7 +370,14 @@ def tokenise(smiles):
             queue.append(('eb', ')'))
         elif char.isdigit() :
             queue.append(('ring',char))
-    return queue                                      
+    return queue              
+
+
+
+'''
+        Fetching SMILES string from a Given IUPAC name using an API
+'''
+
 def convert(ids):
     try:
         url = f"http://cactus.nci.nih.gov/chemical/structure/{ids}/smiles"
@@ -185,95 +385,38 @@ def convert(ids):
         return ans
     except Exception as e:
         l.fatal({traceback.print_exc()})  # haven't tested this yet lol
-def smile(z,g):
-    anchor = None
-    index = 0
-    next_bond = None
-    branches= []
-    default = 1
-    stack = tokenise(z)
-    rings = {}
-    for tokentype,token in stack:
-        if tokentype == 'atom':
-            g.add_atom(atom(index,token))
-            if anchor is not None:
-                if next_bond is None:
-                    next_bond = default
-                if next_bond:
-                    g.add_bond(g.get_atom(anchor),g.get_atom(index), next_bond)
-                next_bond = None
-            anchor = index
-            index += 1
-        elif tokentype == 'sb':
-            branches.append(anchor)
-        elif tokentype == 'eb':
-            anchor = branches.pop()
-        elif tokentype == 'bond':
-            next_bond = token
-        elif tokentype == 'ring':
-            if token in rings:
-                jdx, order = rings[token]
-                if next_bond is None and order is None:  
-                    next_bond = 1
-                elif order is None:
-                    next_bond = 1
-                elif next_bond is None:
-                    next_bond = order
-                elif next_bond != order:
-                    print("error")
-                    break
-                elif int(index - 1) == jdx:
-                    print("error2")
-                if next_bond is not None:
-                    g.add_bond(g.get_atom(index-1),g.get_atom(jdx),next_bond)
-                    next_bond = None
-                next_bond = None
-                del rings[token]
-            else:
-                rings[token] = (index-1,next_bond)     
-    return g.add_hydrogens()
+        
 
 
 
-
-
-z = 'CC(=O)OC'
 
 '''
-random.choice(['NNccc(ccc[N+]([O-])=O)[N+]([O-])=O',
-                   '[Cu+2].[O-]S(=O)(=O)[O-]',
-                   'OCCc1c(C)[n+](cs1)Cc2cnc(C)nc2N',
-                   'COc1cc(C=O)ccc1O',
-                   'CC(=O)OC1=CC=CC=C1C(=O)O',
-                   'C(C1C(C(C(C(O1)O)O)O)O)O',
-                   'C(I)(I)I',
-                   'CCOCC',
-                   'c1ccccc1', # need to kekulize this benzene. Current output results in all the bonds being order 1
-                   'C1CCCCC1',
-                   'C#N',
-                   'NC(Cl)(Br)C(=O)O', #canonical
-                   'O=C(O)C(N)(Br)Cl' #non canonical
-                   ])
 
-                '''
-g = molecule()
-print(z)
-smile(z,g)
-lol = g.schema()
-pprint(lol)
-g.carboxyl()
+Main Prompt
 
 '''
-lol = g.graph()
-zzz = g.schema()
-print(json.dumps(lol, indent=2, default=str))
-pprint(zzz)
-G = nx.Graph(lol)
-pos=nx.spring_layout(G, k=0.3*1/np.sqrt(len(g.nodes())), iterations=500) 
-nx.draw_networkx(G,pos)
-labels = nx.get_edge_attributes(G,'bond-order')
-nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-plt.axis('off')
-plt.draw()
-plt.savefig("graph.pdf")
-'''
+
+def main():
+
+
+    ids = input("Enter the IUPAC name of a molecule")
+    print('You have entered: ', ids)
+    z = convert(ids)
+    print('SMILES Format: ', z)
+    g = molecule()
+    g.smile(z)
+    a = g.graph() #index notation
+    print('Your molecule is saved as graph.pdf')
+    print(json.dumps(a, indent=2, default=str)) #printing the graph notation in JSON format
+    G = nx.Graph(a)
+    pos=nx.spring_layout(G, k=0.3*1/np.sqrt(len(g.nodes())), iterations=500)
+    nx.draw_networkx(G,pos)
+    labels = nx.get_edge_attributes(G,'bond-order')
+    nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+    plt.axis('off')
+    plt.draw()
+    plt.savefig("graph.pdf")
+	
+
+if __name__ == "__main__":
+    main()
