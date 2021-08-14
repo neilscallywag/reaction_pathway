8702bff70822db8172064866181ef0ad3ea88614
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-08-14 20:04:43 +0800

    Added Functional groups

--- main.py
+++ main.py
@@ -19,6 +19,30 @@ group_16 = ['O','o','S','s','Se']
 group_17 = ['F','Cl','Br','I']
 
 
+
+'''
+    Atomic massses
+
+'''
+atomic_mass = {'H': 1.00794, 'He': 4.002602, 'Li': 6.941, 'Be': 9.012182, 'B': 10.811, 'C': 12.0107, 'N': 14.0067,
+              'O': 15.9994, 'F': 18.9984032, 'Ne': 20.1797, 'Na': 22.98976928, 'Mg': 24.305, 'Al': 26.9815386,
+              'Si': 28.0855, 'P': 30.973762, 'S': 32.065, 'Cl': 35.453, 'Ar': 39.948, 'K': 39.0983, 'Ca': 40.078,
+              'Sc': 44.955912, 'Ti': 47.867, 'V': 50.9415, 'Cr': 51.9961, 'Mn': 54.938045,
+              'Fe': 55.845, 'Co': 58.933195, 'Ni': 58.6934, 'Cu': 63.546, 'Zn': 65.409, 'Ga': 69.723, 'Ge': 72.64,
+              'As': 74.9216, 'Se': 78.96, 'Br': 79.904, 'Kr': 83.798, 'Rb': 85.4678, 'Sr': 87.62, 'Y': 88.90585,
+              'Zr': 91.224, 'Nb': 92.90638, 'Mo': 95.94, 'Tc': 98.9063, 'Ru': 101.07, 'Rh': 102.9055, 'Pd': 106.42,
+              'Ag': 107.8682, 'Cd': 112.411, 'In': 114.818, 'Sn': 118.71, 'Sb': 121.760, 'Te': 127.6,
+              'I': 126.90447, 'Xe': 131.293, 'Cs': 132.9054519, 'Ba': 137.327, 'La': 138.90547, 'Ce': 140.116,
+              'Pr': 140.90465, 'Nd': 144.242, 'Pm': 146.9151, 'Sm': 150.36, 'Eu': 151.964, 'Gd': 157.25,
+              'Tb': 158.92535, 'Dy': 162.5, 'Ho': 164.93032, 'Er': 167.259, 'Tm': 168.93421, 'Yb': 173.04,
+              'Lu': 174.967, 'Hf': 178.49, 'Ta': 180.9479, 'W': 183.84, 'Re': 186.207, 'Os': 190.23, 'Ir': 192.217,
+              'Pt': 195.084, 'Au': 196.966569, 'Hg': 200.59, 'Tl': 204.3833, 'Pb': 207.2, 'Bi': 208.9804,
+              'Po': 208.9824, 'At': 209.9871, 'Rn': 222.0176, 'Fr': 223.0197, 'Ra': 226.0254, 'Ac': 227.0278,
+              'Th': 232.03806, 'Pa': 231.03588, 'U': 238.02891, 'Np': 237.0482, 'Pu': 244.0642, 'Am': 243.0614,
+              'Cm': 247.0703, 'Bk': 247.0703, 'Cf': 251.0796, 'Es': 252.0829, 'Fm': 257.0951, 'Md': 258.0951,
+              'No': 259.1009, 'Lr': 262, 'Rf': 267, 'Db': 268, 'Sg': 271, 'Bh': 270, 'Hs': 269, 'Mt': 278,
+              'Ds': 281, 'Rg': 281, 'Cn': 285, 'Nh': 284, 'Fl': 289, 'Mc': 289, 'Lv': 292, 'Ts': 294, 'Og': 294}
+
 '''
     Defining an Atom:
                      An Atom for our purpose is defined as an object with the
@@ -28,6 +52,7 @@ group_17 = ['F','Cl','Br','I']
                      3. Its Valency - > valency: int
                      4. Its Charge -> Charge: undefined
                      5. If the atom is aromatic or not -> aromatic: Boolean
+                     6. Its atomic mass -> ar:int
 
                      Extra Information:
                      Aromaticity is currently defined by lower case strings
@@ -41,6 +66,7 @@ class atom(object):
         self.name = name
         self.valency = self.get_valency(name)
         self.charge = charge
+        self.ar = atomic_mass[name] if name in atomic_mass else 0
         self.aromatic = True if name.islower() else False
     def valency(self):
         return self.valency
@@ -311,7 +337,66 @@ class molecule:
                     rings[token] = (index-1,next_bond)     
         return self.add_hydrogens()
 
-    
+
+    '''
+        Get molecular mass of the molecule
+        
+    '''
+    def molmass(self):
+        mr = 0
+        for atoms in self.molecule.keys():
+            mr += atoms.ar
+        return mr
+
+    '''
+        Function group methods
+    '''
+    def exists(self,fg):
+        valid_fg = ['primary amine','secondary amine', 'tertiary amine', 'primary alcohol', 'secondary alcohol', 'tertiary alcohol', 'carboxylic acid', 'acyl chloride', 'ester', 'halogenoalkane', 'aldehyde', 'ketone']
+        if fg not in valid_fg:
+            return 'Unknown functional group'
+        if fg == valid_fg[0]:                                                           
+            for atom in self.molecule.keys():
+                if atom.name == 'N':
+                    if len(self.molecule[atom]) == 3:
+                        hcount = 0
+                        ccount = 0
+                        others = 0
+                        for tuples in self.molecule[atom]:
+                            if tuples[0].name == 'H':
+                                hcount += 1
+                            elif tuples[0].name == 'C':
+                                ccount += 1
+                            else:
+                                others += 1
+                        if hcount == 2 and ccount == 1 and others == 0:
+                            return True
+                        else:
+                            return False
+                       
+                    else:
+                        return len(self.molecule[atom])
+     
+        elif fg == valid_fg[1]:                                                           
+            for atom in self.molecule.keys():
+                if atom.name == 'N':
+                    if len(self.molecule[atom]) == 3:
+                        hcount = 0
+                        others = 0
+                        for tuples in self.molecule[atom]:
+                            if tuples[0].name == 'H':
+                                hcount += 1
+                            else:
+                                others += 1
+                        if hcount == 1 and others == 2:
+                            return True
+                        else:
+                            return False
+                       
+                    else:
+                        return len(self.molecule[atom])    
+        else:
+            return '!= fg[1]'
 
     '''
                     End Molecule Class
@@ -350,7 +435,7 @@ def tokenise(smiles):
                 peek = None
             else:
                 queue.append(('atom',char))
-        elif char in '-=#$:.':
+        elif char in '-=#$:.@':
             if char == '-':
                 order = 1
             elif char == '=':
@@ -363,6 +448,8 @@ def tokenise(smiles):
                 order = 1.5
             elif char == '.':
                 order = 0
+            elif char == '@':
+                order = 1   
             queue.append(('bond',order))
         elif char == '(':
             queue.append(('sb', '('))
@@ -399,15 +486,19 @@ Main Prompt
 def main():
 
 
-    ids = input("Enter the IUPAC name of a molecule")
+    ids = input("Enter the IUPAC name of a molecule: ")
     print('You have entered: ', ids)
     z = convert(ids)
     print('SMILES Format: ', z)
     g = molecule()
     g.smile(z)
+    print(g.nodes())
     a = g.graph() #index notation
+    b = g.molmass()
+    print('sec amine ?:', g.exists('primary amine'))
+    print('Molecular mass: ',b)
     print('Your molecule is saved as graph.pdf')
-    print(json.dumps(a, indent=2, default=str)) #printing the graph notation in JSON format
+    print(json.dumps(a, indent=2, default=str)) #printing the index notation in JSON format
     G = nx.Graph(a)
     pos=nx.spring_layout(G, k=0.3*1/np.sqrt(len(g.nodes())), iterations=500)
     nx.draw_networkx(G,pos)
